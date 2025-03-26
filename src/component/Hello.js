0d2608737dc0f7ca015b534c5583c4fd0e0d5b3f
Yuyeseul <144925533+Yuyeseul@users.noreply.github.com>
2025-03-26 17:40:56 +0900

    이벤트 처리 연습

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,20 +1,30 @@
-import World from './World';
-import styles from './Hello.module.css';
-
 export default function Hello(){
+    function showName() {
+        console.log('Mike');
+    }
+    function showAge(age){
+        console.log(age);
+    }
+    function showText(txt){
+        console.log(txt);
+    }
+
     return(
         <div>
-            <h1
-                style={{
-                    color: '#f00',
-                    borderRight: '12px solid #000',
-                    marginBottom: '50px',
-                    opacity: 1,
+            <h1>state</h1>
+            <h2>컴포넌트의 속성값</h2>
+            <button onClick={showName}>show name</button>
+            <button 
+                onClick={()=>{
+                    showAge(10);
                 }}
             >
-                Hello
-            </h1>
-            <div className={styles.box}> Hello</div>
+                show age
+            </button>
+            <input type='text' onChange={e =>{
+                const txt = e.target.value;
+                showText(txt);
+            }}/>
         </div>
     );
 }
