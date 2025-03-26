5342d06148a8280bfa5cbe09e4f18fdd4b5fb326
Yuyeseul <144925533+Yuyeseul@users.noreply.github.com>
2025-03-26 21:05:23 +0900

    state, props 연습
    state -> 컴포넌트의 상태를 의미하고, 상태가 변경되면 컴포넌트가 다시 렌더링 
    
    useState -> 컴포넌트가 기억하고자 하는 값, 즉 state 를 컴포넌트에 추가할 수 있도록 해주는 React Hook 
    const [현재 state, state 를 변경하기 위한 함수] = useState();
    
    props - > 부모 컴포넌트에서 자식 컴포넌트로 값을 전달할 때 사용.  읽기 전용, 자식 컴포넌트에서 props 수정 불가

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,30 +1,18 @@
-export default function Hello(){
-    function showName() {
-        console.log('Mike');
-    }
-    function showAge(age){
-        console.log(age);
-    }
-    function showText(txt){
-        console.log(txt);
-    }
+import { useState } from 'react';
+import UserName from './UserName';
+
+export default function Hello({age}){
+    
+    const [name, setName] = useState('Mike');
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
     return(
         <div>
-            <h1>state</h1>
-            <h2>컴포넌트의 속성값</h2>
-            <button onClick={showName}>show name</button>
-            <button 
-                onClick={()=>{
-                    showAge(10);
-                }}
-            >
-                show age
-            </button>
-            <input type='text' onChange={e =>{
-                const txt = e.target.value;
-                showText(txt);
-            }}/>
+            <h2 id='name'>{name}({age}) : {msg}</h2>
+            <UserName name={name} /> 
+            <button onClick={()=>{
+                setName(name === 'Mike' ? 'Jane' : 'Mike')
+            }}>Change</button>
         </div>
     );
 }
