cfd8ee742eb11a818e13c8510af303f2c4fa68da
Yuyeseul <144925533+Yuyeseul@users.noreply.github.com>
2025-03-27 15:44:47 +0900

    라우터 구현 react-router-dom
    BrowserRouter, Route, Switch

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,13 +1,12 @@
 import dummy from '../db/data.json';
+import { useParams } from 'react-router-dom';
 
 export default function Day() {
-  //dummy.words
-  const day = 2;
-  const wordList = dummy.words.filter((word) => word.day === day);
-  console.log(wordList);
-
+  const { day } = useParams(); //문자자
+  const wordList = dummy.words.filter((word) => word.day === Number(day)); //day 는 숫자라 Number
   return (
     <>
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (
